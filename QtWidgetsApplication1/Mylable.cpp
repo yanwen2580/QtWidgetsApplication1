5df5d042745c177c869705fabcce27cbbadf2fe8
文 晏 <2714403672@qq.com>
2023-05-07 16:52:44 +0800

    11

--- QtWidgetsApplication1/Mylable.cpp
+++ QtWidgetsApplication1/Mylable.cpp
@@ -5,6 +5,7 @@
 #include <qwidget.h>
 #include <QLabel>
 #include <qmessagebox.h>
+#include <QMouseEvent>
 
 Mylable::Mylable(QWidget* parent):QLabel(parent)
 {
@@ -16,8 +17,8 @@ Mylable::~Mylable()
  }
 void Mylable::enterEvent(QEnterEvent* event)
 {
-	QMessageBox *box = new QMessageBox(this);
-	box->information(this,"222！","鼠标进来了");
+	/*QMessageBox *box = new QMessageBox(this);
+	box->information(this,"222！","鼠标进来了");*/
 }
 void Mylable::leaveEvent(QEvent* event)
 {
@@ -28,16 +29,30 @@ void Mylable::leaveEvent(QEvent* event)
 
 void Mylable::mousePressEvent(QMouseEvent* event)
 {
+	
+	/*QMessageBox* box = new QMessageBox(this);
+	QString str =  QString("鼠标按下了位置在 %1 %2").arg(event->x()).arg(event->y());
+	box->information(this, "222！",str);*/
+
 }
 
 void Mylable::mouseReleaseEvent(QMouseEvent* event)
 {
+	/*QMessageBox* box = new QMessageBox(this);
+	QString str = QString("鼠标释放了位置在 %1 %2").arg(event->x()).arg(event->y());
+	box->information(this, "222！", str);*/
 }
 
 void Mylable::mouseDoubleClickEvent(QMouseEvent* event)
-{
+{/*
+	QMessageBox* box = new QMessageBox(this);
+	QString str = QString("鼠标双击了位置在 %1 %2").arg(event->x()).arg(event->y());
+	box->information(this, "222！", str);*/
 }
 
 void Mylable::mouseMoveEvent(QMouseEvent* event)
 {
+	QMessageBox* box = new QMessageBox(this);
+	QString str = QString("追踪鼠标事件 %1 %2").arg(event->x()).arg(event->y());
+	box->information(this, "222！", str);
 }
